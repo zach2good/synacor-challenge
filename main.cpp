8be4892df1e00f13b6b014cc5dda7d693ce278bc
Zach Toogood <zachary.toogood@concentra.co.uk>
2015-12-10 17:06:22 +0000

    Fixed dead module problems

---
+++ main.cpp
@@ -0,0 +1,242 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stack>
+#include <algorithm>
+
+#include <SDL/SDL.h>
+
+#include "Window.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_sdl.h"
+#define IM_ARRAYSIZE(_ARR)  ((int)(sizeof(_ARR)/sizeof(*_ARR)))
+
+#include "console.h"
+
+#include "vm.h"
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+
+using namespace std;
+
+// SDL Window 
+auto window = Window("Synacor VM", 1280, 720);
+
+// VM
+VM vm = VM();
+
+bool im_openMemoryEditor = true; 
+bool im_openRegisterEditor = true; 
+bool im_openStackEditor = true; 
+bool im_openConsole = true;
+
+bool run_vm = false;
+
+static char input_buffer[64];
+
+string digestPreview;
+ 
+void main_loop()
+{
+    // Handle Input
+    SDL_Event event;  
+    while (SDL_PollEvent(&event))
+    {
+    	ImGui_ImplSdl_ProcessEvent(&event);
+    }
+
+    // Update
+    if (vm.isRunning)
+    {
+        vm.step();
+    }
+
+    // Prepare GUI
+    ImGui_ImplSdl_NewFrame(window.getWindow());
+    static Console console;
+
+    if (im_openConsole)
+    {
+        console.Draw("Console", &im_openConsole);
+        console.UpdateDigestBuffer(vm.digest);
+
+        if (vm.stringDigest)
+        {
+            console.AddLog(vm.digest.c_str());
+            digestPreview = vm.digest;
+            vm.digest = "";
+            vm.stringDigest = false;
+        }
+    }   
+
+    ImGui::SetNextWindowSize(ImVec2(250,400), ImGuiSetCond_FirstUseEver);
+    ImGui::SetNextWindowPos(ImVec2(30, 30), ImGuiSetCond_FirstUseEver);
+
+    ImGui::Begin("Synacor VM");
+
+        if (vm.isRunning)
+        {
+           ImGui::Text("Status: VM Running"); 
+        }
+        else
+        {
+            ImGui::Text("Status: VM Halted"); 
+        }
+            
+        ImGui::Checkbox("Debug", vm.getDebug());
+        ImGui::Text("Instruction Count: %d", vm.getInstructionCount()); 
+        ImGui::Text("Memory Ptr: %d", vm.getMemoryPtr()); 
+        ImGui::Text("Self-Test ends at 701400 instructions"); 
+        
+        if (ImGui::Button("Step"))
+        {
+            vm.step();
+        } 
+
+        if (!vm.isRunning)
+        {
+           if (ImGui::Button("Run VM")) vm.isRunning = true; 
+
+        }   
+        else
+        {
+            if (ImGui::Button("Pause VM")) vm.isRunning = false;
+        }
+        ImGui::Text(""); 
+
+        if (ImGui::Button("Console")) im_openConsole = !im_openConsole;
+        if (ImGui::Button("Memory Editor")) im_openMemoryEditor = !im_openMemoryEditor;
+        if (ImGui::Button("Stack Editor")) im_openStackEditor = !im_openStackEditor;
+        if (ImGui::Button("Register Editor")) im_openRegisterEditor = !im_openRegisterEditor;
+
+        ImGui::Text(""); 
+
+        if (ImGui::Button("Restore State (dump_695700.bin)")) vm.restoreState("dump_695700.bin");
+        
+        if (ImGui::Button("Help"))
+        {
+            console.AddLog("0> 21 OP_NOOP\n");
+            console.AddLog("1> 21 OP_NOOP\n");
+            console.AddLog("2> 19 OP_OUT W\n");
+            console.AddLog("4> 19 OP_OUT e\n");
+            console.AddLog("6> 19 OP_OUT l\n");
+            console.AddLog("8> 19 OP_OUT c\n");
+            console.AddLog("10> 19 OP_OUT o\n");
+            console.AddLog("12> 19 OP_OUT m\n");
+            console.AddLog("14> 19 OP_OUT e\n");
+        } 
+        
+    ImGui::End();
+
+    if (im_openRegisterEditor)
+    {
+        ImGui::SetNextWindowSize(ImVec2(250,255), ImGuiSetCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(30,430), ImGuiSetCond_FirstUseEver);
+
+        ImGui::Begin("Register Editor");
+            ImGui::Columns(2, "Registers");
+            ImGui::Separator();
+            ImGui::Text("Address"); ImGui::NextColumn();
+            ImGui::Text("Value"); ImGui::NextColumn();
+            ImGui::Separator();
+            ImGui::Separator();
+
+            for (int i = 0; i < 8; ++i)
+            {
+                ImGui::Text("R%d", i); ImGui::NextColumn();
+                ImGui::Text("%d", vm.getRegisters()[i]); ImGui::NextColumn();
+                ImGui::Separator();
+            }
+        ImGui::End();
+    }
+
+    if (im_openStackEditor)
+    {
+        ImGui::SetNextWindowSize(ImVec2(200,400), ImGuiSetCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(280,30), ImGuiSetCond_FirstUseEver);
+
+        auto stack = vm.getStack(); 
+        vector<ushort> v = vector<ushort>();
+        while(!stack.empty())
+        {
+            v.push_back(stack.top());
+            stack.pop();
+        }
+
+
+        ImGui::Begin("Stack Editor");
+            ImGui::Columns(2, "Stack");
+            ImGui::Separator();
+            ImGui::Text("Depth"); ImGui::NextColumn();
+            ImGui::Text("Value"); ImGui::NextColumn();
+            ImGui::Separator();
+            ImGui::Separator();
+
+            for (int i = 0; i < v.size(); ++i)
+            {
+                ImGui::Text("%d", i); ImGui::NextColumn();
+                ImGui::Text("%d", v.at(i)); ImGui::NextColumn();
+                ImGui::Separator();
+            }
+
+        ImGui::End();
+    }
+
+    if (im_openMemoryEditor)
+    {
+        ImGui::SetNextWindowSize(ImVec2(250,400), ImGuiSetCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(480,30), ImGuiSetCond_FirstUseEver);
+
+        auto mem = vm.getMemory();
+        auto memPtr = vm.getMemoryPtr();
+
+        ImGui::Begin("Memory Editor");
+        
+        ImGui::Columns(2, "Memory");
+        ImGui::Separator();
+
+        ImGui::Text("Memory Ptr"); ImGui::NextColumn();
+        ImGui::Text("%d", memPtr); ImGui::NextColumn();
+
+        ImGui::Separator();
+        ImGui::Separator();
+
+        ImGui::Text("Memory pos"); ImGui::NextColumn();
+        ImGui::Text("Value"); ImGui::NextColumn();
+        ImGui::Separator();
+        ImGui::Separator();
+
+        for (int i = 0; i < 100; ++i)
+        {
+            ImGui::Text("%d", i); ImGui::NextColumn();
+            ImGui::Text("%d", mem->at(i)); ImGui::NextColumn();
+            ImGui::Separator();
+        }
+            
+        ImGui::End();
+    } 
+
+    // Render
+    window.clear();
+    ImGui::Render();
+    window.swap();
+}
+
+int main() {
+	
+	ImGui_ImplSdl_Init(window.getWindow());
+
+ 	emscripten_set_main_loop(main_loop, 10000, 1);
+
+  	ImGui_ImplSdl_Shutdown();
+
+	return 0;
+}
+
---
+++ vm.cpp
@@ -0,0 +1,615 @@
+#include "vm.h"
+
+VM::VM()
+{
+	init();
+}
+
+VM::~VM()
+{
+
+}
+
+void VM::init()
+{
+	m_memory = loadBin("fs/challenge.bin");
+}
+
+std::string VM::openFile(std::string input, bool newLine = true)
+{
+	string temp;
+	string content;
+	ifstream file(input);
+	if (file.is_open())
+	{
+		while (getline(file, temp))
+		{
+			if (newLine)
+				content += temp + "\n";
+			else
+				content += temp;
+		}
+		file.close();
+	}
+
+	else cout << "Unable to open file";
+
+	return content;
+}
+
+std::vector<std::string> VM::split(const std::string &s, const std::string &delims)
+{
+	std::vector<std::string> result;
+	std::string::size_type pos = 0;
+	while (std::string::npos != (pos = s.find_first_not_of(delims, pos))) {
+		auto pos2 = s.find_first_of(delims, pos);
+		result.emplace_back(s.substr(pos, std::string::npos == pos2 ? pos2 : pos2 - pos));
+		pos = pos2;
+	}
+	return result;
+}
+
+
+bool VM::isRegister(ushort s)
+{
+	return s >= 32768 && s <= 32775;
+}
+
+ushort VM::getRegister(ushort s)
+{
+	return s - 32768;
+}
+
+uchar VM::toAscii(ushort s)
+{
+	return (uchar)s;
+}
+
+vector<ushort> VM::loadBin(string input)
+{
+	int length;
+	uchar * buffer = nullptr;
+
+	ifstream file(input, ios::in | ios::binary);
+	if (file.is_open())
+	{
+		// get length of file:
+		file.seekg(0, ios::end);
+		length = file.tellg();
+		file.seekg(0, ios::beg);
+
+		// allocate memory:
+		buffer = new uchar[length];
+
+		file.read((char*)buffer, length);
+		file.close();
+	}
+	else
+	{
+		//cout << "Unable to open file" << endl;
+	}
+
+	vector<ushort> codes = vector<ushort>();
+
+	for (int i = 0; i < length / 2; i++)
+	{
+		ushort c = ((ushort*)buffer)[i];
+		codes.push_back(c);
+	}
+
+	return codes;
+}
+
+ushort VM::value(ushort s)
+{
+	if (isRegister(s))
+	{
+		return m_reg[getRegister(s)];
+	}
+	return s;
+}
+
+void VM::step() {
+	handleOP(m_memory.at(memPtr));
+}
+
+void VM::run() {
+
+	while (isRunning)  // Input
+	{
+		step();
+	}
+}
+
+void VM::handleOP(int op)
+{
+	ushort size = m_memory.size();
+
+	currentOp = op;
+
+	if (debug)
+	{
+		digest += format("%d> ", memPtr);
+	}
+
+	switch (op)
+	{
+	case OP_HALT: // 0
+	{
+		// stop execution and terminate the program
+		if (debug) digest += format("%d OP_HALT\n", op);
+		memPtr = size;
+		break;
+	}
+
+	case OP_SET: // 1 a b
+	{
+		// set register <a> to the value of <b>
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		if (debug) digest += format("%d OP_SET: R%d %d\n", op, m_reg[a], b);
+		m_reg[a] = b;
+
+		memPtr += 2;
+		break;
+	}
+
+	case OP_PUSH: // 2 a
+	{
+		// push <a> onto the stack
+		a = value(m_memory.at(memPtr + 1));
+		if (debug) digest += format("%d OP_PUSH: %d\n", op, a);
+
+		m_stack.push(a);
+
+		memPtr += 1;
+		break;
+	}
+
+	case OP_POP: // 3 a
+	{
+		// remove the top element from the stack and write it into <a>; empty stack = error
+		a = getRegister(m_memory.at(memPtr + 1));
+		if (debug) digest += format("%d OP_POP: R%d\n", op, a);
+
+		if (m_stack.empty())
+		{
+			digest += format("Empty Stack!\n");
+			memPtr = size;
+			break;
+		}
+		else
+		{
+			m_reg[a] = m_stack.top();
+			m_stack.pop();
+
+			memPtr += 1;
+			break;
+		}
+	}
+
+	case OP_EQ: // 4 a b
+	{
+		// set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_EQ: R%d %d %d\n", op, a, b, c);
+
+		if (b == c)
+		{
+			m_reg[a] = 1;
+		}
+		else
+		{
+			m_reg[a] = 0;
+		}
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_GT: // 5 a b c
+	{
+		// set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_GT: R%d %d %d\n", op, m_reg[a], b, c);
+
+		if (b > c)
+		{
+			m_reg[a] = 1;
+		}
+		else
+		{
+			m_reg[a] = 0;
+		}
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_JMP: // 6 a
+	{
+		// jump to <a>
+		a = value(m_memory.at(memPtr + 1));
+
+		if (debug) digest += format("%d OP_JMP: %d\n", op, a);
+
+		memPtr = a - 1;
+
+		//i += 1;
+		break;
+	}
+
+	case OP_JT: // 7 a b
+	{
+		// if <a> is nonzero, jump to <b>
+		a = value(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+
+		if (debug) digest += format("%d OP_JT: %d %d\n", op, a, b);
+
+		if (a > 0)
+		{
+			memPtr = b - 1;
+			break;
+		}
+		else
+		{
+			memPtr += 2;
+			break;
+		}
+	}
+
+	case OP_JF: // 8 a b
+	{
+		// if <a> is zero, jump to <b>
+		a = value(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+
+		if (debug) digest += format("%d OP_JF: %d %d\n", op, a, b);
+
+		if (a == 0)
+		{
+			memPtr = b - 1;
+			break;
+		}
+		else
+		{
+			memPtr += 2;
+			break;
+		}
+	}
+
+	case OP_ADD: // 9 a b c
+	{
+		// assign into <a> the sum of <b> and <c> (modulo 32768)
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_ADD: R%d %d %d\n", op, a, b, c);
+
+		m_reg[a] = (b + c) % 32768;
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_MULT: // 10 a b c
+	{
+		// store into <a> the product of <b> and <c> (modulo 32768)
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_MULT: R%d %d %d\n", op, a, b, c);
+
+		m_reg[a] = (b * c) % 32768;
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_MOD: // 11 a b c
+	{
+		// store into <a> the remainder of <b> divided by <c>
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_MOD: R%d %d %d\n", op, a, b, c);
+
+		m_reg[a] = (b % c);
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_AND: // 12 a b c
+	{
+		// stores into <a> the bitwise and of <b> and <c>
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_AND: R%d %d %d\n", op, a, b, c);
+
+		m_reg[a] = b & c;
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_OR: // 13 a b c
+	{
+		// stores into <a> the bitwise or of <b> and <c>
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+		c = value(m_memory.at(memPtr + 3));
+
+		if (debug) digest += format("%d OP_OR: R%d %d %d\n", op, a, b, c);
+
+		m_reg[a] = b | c;
+
+		memPtr += 3;
+		break;
+	}
+
+	case OP_NOT: // 14 a b 
+	{
+		// stores 15-bit bitwise inverse of <b> in <a>
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+
+		if (debug) digest += format("%d OP_NOT: R%d %d\n", op, a, b);
+
+		m_reg[a] = (~b & 32767);
+
+		memPtr += 2;
+		break;
+	}
+
+	case OP_RMEM: // 15 a b 
+	{
+		// read memory at address <b> and write it to <a>
+		a = getRegister(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+
+		if (debug) digest += format("%d OP_RMEM: %d %d\n", op, a, b);
+
+		m_reg[a] = m_memory.at(b);
+
+		memPtr += 2;
+		break;
+	}
+
+	case OP_WMEM: // 16 a b 
+	{
+		// write the value from <b> into memory at address <a>
+		a = value(m_memory.at(memPtr + 1));
+		b = value(m_memory.at(memPtr + 2));
+
+		if (debug) digest += format("%d OP_WMEM: %d %d\n", op, a, b);
+
+		m_memory.at(a) = b;
+
+		memPtr += 2;
+		break;
+	}
+
+	case OP_CALL: // 17 a  
+	{
+		// write the address of the next instruction to the stack and jump to <a>
+		a = value(m_memory.at(memPtr + 1));
+
+		m_stack.push(memPtr + 2);
+
+		memPtr = a - 1;
+
+		if (debug) digest += format("%d OP_CALL: %d\n", op, a);
+
+		break;
+	}
+
+	case OP_RET: // 18  
+	{
+		// remove the top element from the stack and jump to it; empty stack = halt
+		if (m_stack.empty())
+		{
+			digest += format("Empty Stack!\n");
+			memPtr = size;
+			break;
+		}
+		else
+		{
+			val = m_stack.top();
+			m_stack.pop();
+
+			memPtr = val - 1;
+
+			if (debug) digest += format("%d OP_RET (%d)\n", op, val);
+
+			break;
+		}
+	}
+
+	case OP_OUT: // 19 a
+	{
+		// write the character represented by ascii code <a> to the terminal
+		a = m_memory.at(memPtr + 1);
+
+		if (debug) digest += format("%d OP_OUT ", op);
+
+		if (isRegister(a))
+		{
+			digest += format("%c", toAscii(m_reg[getRegister(a)]));
+		}
+		else
+		{
+			digest += format("%c", a);
+		}
+
+		if (debug) digest += '\n';
+
+		memPtr += 1;
+		break;
+	}
+
+	case OP_IN: // 20 a
+	{
+		// read a character from the terminal and write its ascii code to <a>; it can be assumed that once input starts, it will continue until a newline is encountered; 
+		// this means that you can safely read whole lines from the keyboard and trust that they will be fully read
+		a = getRegister(m_memory.at(memPtr + 1));
+
+		if (debug) format("%d OP_IN\n", op);
+
+		uchar input;
+		if (!commands.empty())
+		{
+			input = commands.front();
+			commands.pop_front();
+			printf("%c", input);
+			m_reg[a] = input;
+		}
+		else
+		{
+			commands.push_back('\n');
+			isRunning = false;
+		}
+
+		memPtr += 1;
+		break;	
+	}
+
+	case OP_NOOP: // 21
+	{
+		if (debug) digest += format("%d OP_NOOP\n", op);
+		break;
+	}
+
+	default:
+	{
+		digest += format("OP Code: %d not recognised\n", op);
+		memPtr = size;
+		break;
+	}
+	}
+
+	if (digest[digest.size()-1] == '\n')
+	{
+		stringDigest = true;
+	}
+
+
+	memPtr += 1;
+	instructionCount += 1;
+}
+
+void VM::dumpState()
+{
+	printf("dumpState\n");
+
+	ofstream myfile;
+	myfile.open("fs/dump.bin", ios::out | ios::trunc | ios::binary);
+
+	// dump layout
+	// registers 0-7
+	// stack size
+	// stack contents
+	// memory size
+	// memory contents
+	// currentOp
+	// memPtr
+	// instructionCount
+
+	for (int i = 0; i < 8; i++)
+	{
+		myfile << m_reg[i] << endl;
+	}
+
+	int stack_size = m_stack.size();
+	myfile << stack_size << endl;
+	for (int i = 0; i < stack_size; i++)
+	{
+		myfile << m_stack.top() << endl;
+		m_stack.pop();
+	}
+
+	myfile << m_memory.size() << endl;
+	for (int i = 0; i < m_memory.size(); i++)
+	{
+		myfile << m_memory.at(i) << endl;
+	}
+
+	myfile << currentOp << endl;
+	myfile << memPtr << endl;
+	myfile << instructionCount;
+
+	myfile.close();
+
+	m_reg[0] = 0;
+}
+
+void VM::restoreState(string input_bin)
+{
+	printf("Restoring state from: %s\n", input_bin.c_str());
+
+	string s = "fs/" + input_bin;
+
+	auto in = split(openFile(s), "\n");
+
+	auto invec = vector<int>();
+	for (int i = 0; i < in.size(); i++)
+	{
+		string str = in.at(i);
+		invec.push_back(stoi(str));
+	}
+
+	// Registers
+	for (int i = 0; i < 8; i++)
+	{
+		m_reg[i] = invec.at(i);
+	}
+
+	// Stack
+	int stackSize = invec.at(8);
+	int stackStart = 9;
+	for (int i = 0; i < stackSize; i++)
+	{
+		// TODO: These are in the wrong order
+		m_stack.push(invec.at(stackStart + i));
+	}
+
+	reverseStack();
+	m_stack = temp_stack;
+
+	// Memory
+	int memorySize = invec.at(stackStart + stackSize);
+	int memoryStart = stackStart + stackSize + 1;
+	for (int i = 0; i < memorySize; i++)
+	{
+		m_memory.at(i) = invec.at(memoryStart + i);
+	}
+
+	int end = invec.size();
+	currentOp = invec.at(end - 3);
+	memPtr = invec.at(end - 2);
+	instructionCount = invec.at(end - 1);
+}
+
+void VM::sendInput(string inputStr)
+{
+	int i_sz = inputStr.size();
+
+	commands.clear();
+	for (int i = 0; i < i_sz; i++)
+	{
+		commands.push_back(inputStr[i]);
+	}
+	//commands.push_back('\n');
+}
